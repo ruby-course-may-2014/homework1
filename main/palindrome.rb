006dbcb4ba27eb89b4cc9f45c9224cfab01aaf17
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2014-05-23 17:40:40 +0300

    Palindrome sceleton and test.

---
+++ main/palindrome.rb
@@ -0,0 +1,7 @@
+module Palindrome
+  extend self
+
+  def test?(str)
+    false
+  end
+end
--- test/palindrome_test.rb
+++ test/palindrome_test.rb
@@ -1,7 +1,22 @@
 require 'minitest/autorun'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'main', 'palindrome'))
 
-class TestOk < MiniTest::Unit::TestCase
-  def test_works
-    assert_equal 2*2, 4
+class TestPalindrome < MiniTest::Unit::TestCase
+  def test_non_palindrome
+    assert !Palindrome.test?('adam'), 'adam should not be palindrome'
+  end
+
+  def test_palindrome
+    assert !Palindrome.test?('redivider'), 'redivider should be palindrome'
+  end
+
+  def test_with_capitals
+    assert Palindrome.test?('reDivider'), 'redivider should be palindrome even with capitals'
+    assert !Palindrome.test?('adAm'), 'adam should not be palindrome even with capitals'
+  end
+
+  def test_non_word
+    assert Palindrome.test?('redi vi, der'), 'redivider should be palindrome even with non-words'
+    assert !Palindrome.test?('ad A ?m'), 'adam should not be palindrome even with non-words'
   end
 end
